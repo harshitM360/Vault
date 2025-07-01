6688430d5d0438f95dcd4e10288a4fe46eb02b9c
harshitM360 <135355659+harshitM360@users.noreply.github.com>
2025-07-01 10:54:49 +0530

    Create Template.cpp

---
+++ Day 2/Template.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define fast_io() ios::sync_with_stdio(false); cin.tie(0);
+#define all(x) x.begin(), x.end()
+
+int32_t main() {
+    fast_io();
+    // solve here
+}
